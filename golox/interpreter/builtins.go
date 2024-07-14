c149a5d5f5995a31ef91a534d51208ae0dd21828
Marcus Caisey <marcus@teckna.com>
2024-07-14 14:17:19 +0100

    style: fix golangci-lint warnings

--- golox/interpreter/builtins.go
+++ golox/interpreter/builtins.go
@@ -5,7 +5,7 @@ import "time"
 var builtinFns = map[string]loxObject{
 	"clock": loxBuiltinFunction{
 		name: "clock",
-		fn: func(args []loxObject) loxObject {
+		fn: func([]loxObject) loxObject {
 			return loxNumber(float64(time.Now().UnixNano()) / float64(time.Second))
 		},
 	},
