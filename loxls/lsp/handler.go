a66ca549242cb5c5dc76fda4b7c1c129b84d49a6
Marcus Caisey <marcus@teckna.com>
2024-11-29 21:32:05 +0000

    feat(loxls): implement stub text document sync methods

--- loxls/lsp/handler.go
+++ loxls/lsp/handler.go
@@ -28,7 +28,7 @@ var (
 )
 
 // HandleRequest responds to a JSON-RPC request.
-func (h *Handler) HandleRequest(method string, params *json.RawMessage) (any, error) {
+func (h *Handler) HandleRequest(method string, jsonParams *json.RawMessage) (any, error) {
 	if !h.initialized && method != "initialize" {
 		return nil, notInitializedErr
 	}
@@ -37,11 +37,11 @@ func (h *Handler) HandleRequest(method string, params *json.RawMessage) (any, er
 	}
 	switch method {
 	case "initialize":
-		var initializeParams *protocol.InitializeParams
-		if err := json.Unmarshal(*params, &initializeParams); err != nil {
+		var params *protocol.InitializeParams
+		if err := json.Unmarshal(*jsonParams, &params); err != nil {
 			return nil, jsonrpc.NewError(jsonrpc.InvalidParams, "Invalid params", map[string]any{"error": err.Error()})
 		}
-		return h.initialize(initializeParams)
+		return h.initialize(params)
 	case "shutdown":
 		return h.shutdown()
 	default:
@@ -50,7 +50,7 @@ func (h *Handler) HandleRequest(method string, params *json.RawMessage) (any, er
 }
 
 // HandleNotification responds to a JSON-RPC notification.
-func (h *Handler) HandleNotification(method string, _ *json.RawMessage) error {
+func (h *Handler) HandleNotification(method string, jsonParams *json.RawMessage) error {
 	if !h.initialized && method != "initialized" && method != "exit" {
 		return notInitializedErr
 	}
@@ -61,6 +61,24 @@ func (h *Handler) HandleNotification(method string, _ *json.RawMessage) error {
 	case "initialized":
 		// No further initialisation needed
 		return nil
+	case "textDocument/didOpen":
+		var params *protocol.DidOpenTextDocumentParams
+		if err := json.Unmarshal(*jsonParams, &params); err != nil {
+			return jsonrpc.NewError(jsonrpc.InvalidParams, "Invalid params", map[string]any{"error": err.Error()})
+		}
+		return h.textDocumentDidOpen(params)
+	case "textDocument/didChange":
+		var params *protocol.DidChangeTextDocumentParams
+		if err := json.Unmarshal(*jsonParams, &params); err != nil {
+			return jsonrpc.NewError(jsonrpc.InvalidParams, "Invalid params", map[string]any{"error": err.Error()})
+		}
+		return h.textDocumentDidChange(params)
+	case "textDocument/didClose":
+		var params *protocol.DidCloseTextDocumentParams
+		if err := json.Unmarshal(*jsonParams, &params); err != nil {
+			return jsonrpc.NewError(jsonrpc.InvalidParams, "Invalid params", map[string]any{"error": err.Error()})
+		}
+		return h.textDocumentDidClose(params)
 	case "exit":
 		return h.exit()
 	default:
@@ -70,12 +88,19 @@ func (h *Handler) HandleNotification(method string, _ *json.RawMessage) error {
 
 func (h *Handler) initialize(*protocol.InitializeParams) (*protocol.InitializeResult, error) {
 	h.initialized = true
-	// TODO: populate this result properly
 	return &protocol.InitializeResult{
-		Capabilities: protocol.ServerCapabilities{},
+		Capabilities: protocol.ServerCapabilities{
+			PositionEncoding: ptrTo(protocol.PositionEncodingKindUTF16),
+			TextDocumentSync: &protocol.TextDocumentSyncKindOrTextDocumentSyncOptions{
+				Value: protocol.TextDocumentSyncOptions{
+					OpenClose: ptrTo(protocol.Boolean(true)),
+					Change:    ptrTo(protocol.TextDocumentSyncKindFull),
+				},
+			},
+		},
 		ServerInfo: &protocol.InitializeResultServerInfo{
 			Name:    "loxls",
-			Version: pointerTo(protocol.String(version)),
+			Version: ptrTo(protocol.String(version)),
 		},
 	}, nil
 }
@@ -94,6 +119,18 @@ func (h *Handler) exit() error {
 	return nil
 }
 
-func pointerTo[T any](v T) *T {
+func (h *Handler) textDocumentDidOpen(*protocol.DidOpenTextDocumentParams) error {
+	return nil
+}
+
+func (h *Handler) textDocumentDidChange(*protocol.DidChangeTextDocumentParams) error {
+	return nil
+}
+
+func (h *Handler) textDocumentDidClose(*protocol.DidCloseTextDocumentParams) error {
+	return nil
+}
+
+func ptrTo[T any](v T) *T {
 	return &v
 }
