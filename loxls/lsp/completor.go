1af79e34c2d34a972280490e03d640d4e1fb676c
Marcus Caisey <marcus@teckna.com>
2025-11-22 15:50:05 +0000

    fix(loxls): reduce snippet indent size from 4 to 2

--- loxls/lsp/completor.go
+++ loxls/lsp/completor.go
@@ -19,11 +19,11 @@ var (
 	statementSnippets  = []statementSnippet{
 		{"print", "print $0;", "print statement", "Snippet for a print statement"},
 		{"var", "var $0;", "variable declaration", "Snippet for a variable declaration"},
-		{"if", "if ($1) {\n    $0\n}", "if statement", "Snippet for an if statement"},
-		{"while", "while ($1) {\n    $0\n}", "while loop", "Snippet for a while loop"},
-		{"for", "for ($1;$2;$3) {\n    $0\n}", "for loop", "Snippet for a for loop"},
-		{"fun", "fun $1($2) {\n    $0\n}", "function declaration", "Snippet for a function declaration"},
-		{"class", "class $1 {\n    $0\n}", "class declaration", "Snippet for a class declaration"},
+		{"if", "if ($1) {\n  $0\n}", "if statement", "Snippet for an if statement"},
+		{"while", "while ($1) {\n  $0\n}", "while loop", "Snippet for a while loop"},
+		{"for", "for ($1;$2;$3) {\n  $0\n}", "for loop", "Snippet for a for loop"},
+		{"fun", "fun $1($2) {\n  $0\n}", "function declaration", "Snippet for a function declaration"},
+		{"class", "class $1 {\n  $0\n}", "class declaration", "Snippet for a class declaration"},
 	}
 )
 
--- vscode-lox/CHANGELOG.md
+++ vscode-lox/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 [Unreleased]
 
+### Fixed
+
+- Reduce snippet indent size from 4 to 2.
+
 ## [2.1.0]
 
 ### Fixed
