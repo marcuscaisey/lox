82132b48dfc850da2db39d84fc56b9d8ad67a12c
Marcus Caisey <marcus@teckna.com>
2026-01-18 18:59:45 +0000

    chore: remove some dev scripts

--- golox/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-golox_dir="$(realpath "$(dirname "$0")")"
-build_path="$golox_dir/../build/golox"
-pushd "$golox_dir" >/dev/null
-go build -o "$build_path" .
-popd >/dev/null
-"$build_path" "$@"
--- test/golox-jlox-compat/jloxify/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-jloxify_dir="$(realpath "$(dirname "$0")")"
-build_path="$jloxify_dir/../../../build/jloxify"
-pushd "$jloxify_dir" >/dev/null
-go build -o "$build_path" .
-popd >/dev/null
-"$build_path" "$@"
